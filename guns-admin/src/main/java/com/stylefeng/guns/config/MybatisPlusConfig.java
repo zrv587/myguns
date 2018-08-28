98da58c0306420a5d2aa7cef1dee9b3cdfef7c6a
zrv587 <1058252138@qq.com>
2018-08-28 17:51:11 +0800

    【新增】mybatis-plus 配置

--- guns-admin/src/main/java/com/stylefeng/guns/config/MybatisPlusConfig.java
+++ guns-admin/src/main/java/com/stylefeng/guns/config/MybatisPlusConfig.java
@@ -3,6 +3,7 @@ package com.stylefeng.guns.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
 import com.stylefeng.guns.core.common.constant.DatasourceEnum;
 import com.stylefeng.guns.core.datascope.DataScopeInterceptor;
 import com.stylefeng.guns.core.datasource.DruidProperties;
@@ -44,6 +45,8 @@ public class MybatisPlusConfig {
         mutiDataSourceProperties.config(dataSource);
         return dataSource;
     }
+
+
 
     /**
      * guns的数据源
@@ -112,5 +115,18 @@ public class MybatisPlusConfig {
     public OptimisticLockerInterceptor optimisticLockerInterceptor() {
         return new OptimisticLockerInterceptor();
     }
+    /***
+     * plus 的性能优化
+     * @return
+     */
+//    @Bean
+//    public PerformanceInterceptor performanceInterceptor() {
+//        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+//        /*<!-- SQL 执行性能分析，开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长 -->*/
+//        performanceInterceptor.setMaxTime(1000);
+//        /*<!--SQL是否格式化 默认false-->*/
+//        performanceInterceptor.setFormat(true);
+//        return performanceInterceptor;
+//    }
 
 }
